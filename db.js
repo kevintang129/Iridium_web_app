a1b67a942c34652476839d87f574af8780fd669e
Kevin Tang <kevintang@airbears2-10-142-34-203.airbears2.1918.berkeley.edu>
2018-12-03 04:41:21 -0800

    webapp

--- db.js
+++ db.js
@@ -1,3 +1,3 @@
 module.exports = {
-  DB: 'mongodb://heroku_93p9nl8v:e5cqvtr2l86i81a32ksehcoact@ds123834.mlab.com:23834/heroku_93p9nl8v'
+  DB: 'mongodb://heroku_93p9nl8v:e5cqvtr2l86i81a32ksehcoact@ds123834.mlab.com:23834/heroku_93p9nl8v/Position'
 }
