bd54a9fa3eafdc1bb655f7d806c9c7a6516e3ea5
Kevin Tang <kevintang@MacBook-Pro-224.local>
2019-01-01 22:00:49 -0800

    first commit

--- PositionRoute.js
+++ PositionRoute.js
@@ -1,3 +1,5 @@
+const express = require('express');
+
 const PositionRouter = express.Router();
 
 const Position = require('./Position');
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var uristring =
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// app.use(express.static(__dirname));
+app.use(express.static(__dirname));
 
 const config = require('./db');
 const PositionRoute = require('./PositionRoute');
